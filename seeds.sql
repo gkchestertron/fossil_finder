54617690be8d8264348d409893ae90d68ad62233
John Fellman <john.fellman@avrs.com>
2015-08-23 16:34:11 -0700

    adds seeds

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+insert into 
+  fossil_finder.fossil_finder_img_tag_categories (name) 
+values 
+  ('Bivalve'),
+  ('Gastropod'),
+  ('Scaphopod'),
+  ('Chiton'),
+  ('Ammonoid'),
+  ('Nautiloid'),
+  ('Belemnoid'),
+  ('Echinoid'),
+  ('Barnacle'),
+  ('Crustacean'),
+  ('Coral'),
+  ('Sponge'),
+  ('Bryozoan');
