b50eb8171597ccef41bbd6f429548e3b015436f3
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2015-10-12 08:11:43 -0700

    move to application

--- application.py
+++ application.py
@@ -8,8 +8,8 @@ from werkzeug import secure_filename
 import os
 
 # Create the Flask application
-app = Flask(__name__)
-app.config.from_object('config')
+application = Flask(__name__)
+application.config.from_object('config')
 
 # create any tables that don't yet exist
 db.create_all()
@@ -19,11 +19,11 @@ def generate_csrf_token():
         session['_csrf_token'] = bcrypt.gensalt()
     return session['_csrf_token']
 
-app.jinja_env.globals['csrf_token'] = generate_csrf_token    
+application.jinja_env.globals['csrf_token'] = generate_csrf_token    
 
 # register assets - any new js must go in here
 # TODO automate finding the files in the tree
-assets = Environment(app)
+assets = Environment(application)
 js = Bundle(
     'js/lib/jquery-1.11.3.js', 
     'js/lib/underscore.js',
@@ -47,23 +47,23 @@ js = Bundle(
 assets.register('js_all', js)
 
 # Register static blueprint
-app.register_blueprint(static.static, session=session, g=g)
+application.register_blueprint(static.static, session=session, g=g)
 
 # Register authentication blueprint
-app.register_blueprint(authentication.authentication, session=session, g=g)
+application.register_blueprint(authentication.authentication, session=session, g=g)
 
 # Register admin blueprint
-app.register_blueprint(admin.admin, session=session, g=g)
+application.register_blueprint(admin.admin, session=session, g=g)
 
 # Register api blueprints
-app.register_blueprint(api.refs, session=session, g=g)
-app.register_blueprint(api.categories, session=session, g=g)
-app.register_blueprint(api.tags, session=session, g=g)
-app.register_blueprint(api.imgs, session=session, g=g)
-app.register_blueprint(api.users, session=session, g=g)
+application.register_blueprint(api.refs, session=session, g=g)
+application.register_blueprint(api.categories, session=session, g=g)
+application.register_blueprint(api.tags, session=session, g=g)
+application.register_blueprint(api.imgs, session=session, g=g)
+application.register_blueprint(api.users, session=session, g=g)
 
 # upload route
-@app.route('/upload', methods=['POST'])
+@application.route('/upload', methods=['POST'])
 def file_upload(search_params=None, **kw):
     user = User.from_token(session.get('token'))
 
@@ -72,9 +72,9 @@ def file_upload(search_params=None, **kw):
 
     file = request.files.get('file')
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['APP_IMAGE_FOLDER'], filename))
+    file.save(os.path.join(application.config['APP_IMAGE_FOLDER'], filename))
     return '', 200
 
 # start the flask loop
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
