32323fe7b8f6949438fef26e48caf447439a4b6b
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2015-10-12 19:09:14 -0700

    removes laziness

--- models/ref.py
+++ models/ref.py
@@ -5,7 +5,7 @@ class Ref(db.Model):
     id                      = db.Column(db.Integer, primary_key = True)
     seq_num                 = db.Column(INTEGER(unsigned=True), index = True, nullable = False, unique = True)
     img                     = db.relationship('Img', primaryjoin='Ref.seq_num==foreign(Img.seq_num)', passive_deletes=True, uselist=False)
-    tags                    = db.relationship('Tag', primaryjoin='Ref.id==foreign(Tag.img_ref_id)', cascade='all', lazy='subquery')
+    tags                    = db.relationship('Tag', primaryjoin='Ref.id==foreign(Tag.img_ref_id)', cascade='all')
     last_accessed_user_id   = db.Column(db.Integer, index = True)
     last_accessed_date_time = db.Column(db.DateTime, index = True)
     completed_by_user_id    = db.Column(db.Integer, index = True)
